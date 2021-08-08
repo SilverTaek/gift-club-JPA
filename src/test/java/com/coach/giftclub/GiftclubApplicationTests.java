9d8f03813161fe92106291e9b819a3185f5e0b5a
rudus1012 <rudus1012@naver.com>
2021-08-08 23:11:39 +0900

    feat:Initial Project

---
+++ src/test/java/com/coach/giftclub/GiftclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coach.giftclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
